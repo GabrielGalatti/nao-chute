c33c8bbcf283de9f6685197216ed165d2a4f1bd4
GabrielGalatti <gabrielgalatti@hotmail.com>
2022-12-23 15:54:12 -0300

    fix: page title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import { useRouter } from "next/router";
 import useSound from "use-sound";
 
@@ -9,18 +10,23 @@ const Home = () => {
   const router = useRouter();
   const [startAudio] = useSound("/sounds/start.mp3");
   return (
-    <HomeTemplate
-      btnLabel="INICIAR QUESTÕES"
-      title="BEM-VINDO À *<br>* *<b>NÃO CHUTE<b>*"
-      description="Queremos que você *<b>se divirta enquanto estuda<b>* para os maiores *<br>* vestibulares do Brasil. *<br>*
+    <>
+      <Head>
+        <title>Não Chute</title>
+      </Head>
+      <HomeTemplate
+        btnLabel="INICIAR QUESTÕES"
+        title="BEM-VINDO À *<br>* *<b>NÃO CHUTE<b>*"
+        description="Queremos que você *<b>se divirta enquanto estuda<b>* para os maiores *<br>* vestibulares do Brasil. *<br>*
         Com *<b>apenas 15 minutos por dias<b>* você assume a liderança na corrida *<br>* para sua tão sonhada vaga"
-      image={inovation}
-      imgAlt="Aprenda jogando!"
-      onClickStart={() => {
-        startAudio();
-        router.push("/start");
-      }}
-    />
+        image={inovation}
+        imgAlt="Aprenda jogando!"
+        onClickStart={() => {
+          startAudio();
+          router.push("/start");
+        }}
+      />
+    </>
   );
 };
 
